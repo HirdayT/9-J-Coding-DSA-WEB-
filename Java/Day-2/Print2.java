056aba17b6718e223ecb0c35c85ddbbf12f5abf9
HirdayT <hirdaytyagi1463@gmail.com>
2026-07-27 13:02:49 +0530

    Updated Classes

---
+++ Java/Day-2/Print2.java
@@ -0,0 +1,12 @@
+class Print2 {
+    public static void main(String[] args)
+    {
+     /*  System.out.print("Padhlo !");
+       System.out.println("\n***");
+       System.out.println(" ** ");
+       System.out.println("  *  ");
+     */
+    }
+     System.out.println("  *  ");
+
+}
