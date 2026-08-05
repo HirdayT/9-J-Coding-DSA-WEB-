7779bca9031f18968d58a491f33fc0f99a26067d
HirdayT <hirdaytyagi1463@gmail.com>
2026-08-05 13:29:01 +0530

    Java

---
+++ Java/Day-4/Variable2.java
@@ -0,0 +1,13 @@
+class Variable2{
+  public static void main(String[] args)
+  {  int age =21 , salary = 200000, token = 38 ;
+ 
+     System.out.println(age) ;
+     System.out.println(salary) ;
+     System.out.println(token) ;
+
+
+
+  }
+
+}
