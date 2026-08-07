2001baee145a43466438429c7e5ec9b730b6e2aa
HirdayT <hirdaytyagi1463@gmail.com>
2026-08-07 14:33:14 +0530

    Datatypes

---
+++ Java/Day-5/Datatypes.java
@@ -0,0 +1,18 @@
+class Datatypes{
+	public static void main(String[] args)
+	{
+           byte rollno ;
+		rollno = 24 ;
+           short totalStudents = 24212 ;
+
+	   long distanceofsun = 32109876532134545l;
+
+			   float salary = 24000000.987f ;
+ 	   System.out.println(rollno);
+	   System.out.println(totalStudents);
+System.out.println(salary);
+System.out.println(distanceofsun);
+	 
+ 	}
+
+}
