d3e5783c2dc6f0b4de785256fb2ef906cd433f0b
HirdayT <hirdaytyagi1463@gmail.com>
2026-08-20 20:43:33 +0530

    Updated Java

---
+++ Java/Day-8/Test.java
@@ -0,0 +1,17 @@
+class Test{
+
+ public static void main(String[] args)
+ {
+  //System.out.println(10 > 20 ) ;// <,>,<=,>=,==
+  int a = 20 ;
+  int b = 20  + 40  ;
+
+ System.out.println( a > b && a == 20  ) ; 
+ System.out.println( a < b || b == 60) ;
+
+ }
+
+
+
+
+}
