4e7c67ba6e3cc415fbd1866b859b2e078b208a8d
HirdayT <hirdaytyagi1463@gmail.com>
2026-09-05 17:42:00 +0530

    Method execution

---
+++ Java/Day-13/Test.java
@@ -0,0 +1,23 @@
+class Test{
+ public static void main(String[] args)
+ {
+     System.out.println("Main method starts");
+      demo();
+     System.out.println("I am well how are you ");   
+      demo();
+      demo();
+     System.out.println("MAin ends");
+  
+ }
+
+  public static void demo(){
+ 	System.out.println("Hi Hope you are well also "); 
+   }
+
+
+
+
+
+
+
+}
---
+++ Java/Day-13/changeArg.java
@@ -0,0 +1,13 @@
+class changeArg{
+
+ public static void main(String[] ram)
+ { 
+   System.out.println("Main starts");
+   System.out.println(5 + 10 + "Do you know 5 + 10 = " +( 5 + 10 ));
+   System.out.println("Main ends"); 
+ } 
+
+
+
+
+}
